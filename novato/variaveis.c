b282259218afa1bc8bbdff325e11a87a1673f896
cvazcosta <cvazcosta2001@gmail.com>
2025-04-18 11:18:09 -0300

    Adicionando o exemplo de declaração e chamada de variável para um único caractere

--- novato/variaveis.c
+++ novato/variaveis.c
@@ -5,6 +5,7 @@ int main () {
   float altura = 1.8; 
   char nome[20] = "Carlos";
   char* sobrenome = "Costa";
-  printf("Meu primeiro nome é: %s\nMeu sobrenome é: %s\nTenho %d anos e %.2f metros de altura\n", nome, sobrenome, idade, altura);
+  char nota = 'A';
+  printf("Meu primeiro nome é: %s\nMeu sobrenome é: %s\nTenho %d anos e %.2f metros de altura\nSou um aluno nota: %c\n", nome, sobrenome, idade, altura, nota);
   return 0;
 }
