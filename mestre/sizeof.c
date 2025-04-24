e55309b55d1fc652a55eeddb6f83508f287c79d8
cvazcosta <cvazcosta2001@gmail.com>
2025-04-24 14:22:00 -0300

    Adicionando medida de tamanho de short int

--- mestre/sizeof.c
+++ mestre/sizeof.c
@@ -6,10 +6,15 @@ int main () {
   ao fato de que ele possui apenas 4 bytes.
   */ 
 
+  // O uso do mod short reduz pela metade a capacidade de armazenamento de int
+
+  printf("Tamanho de short int: %lu bytes\n", sizeof(short int));
   printf("Tamanho de float: %lu bytes\n", sizeof(float));
   printf("Tamanho de int: %lu bytes\n", sizeof(int));
   printf("Tamanho de long int: %lu bytes\n", sizeof(long int));
   printf("Tamanho de long long int: %lu bytes\n", sizeof(long long int));
   printf("Tamanho de double: %lu bytes\n", sizeof(double));
   printf("Tamanho de long double: %lu bytes\n", sizeof(long double));
+
+  return 0;
 }
