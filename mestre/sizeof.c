fc1fa7e9912abbc9d8b0951b105314a3c1fb7234
cvazcosta <cvazcosta2001@gmail.com>
2025-04-28 20:06:27 -0300

    Adicionando o tipo unsigned long int

--- mestre/sizeof.c
+++ mestre/sizeof.c
@@ -12,6 +12,7 @@ int main () {
   printf("Tamanho de float: %lu bytes\n", sizeof(float));
   printf("Tamanho de int: %lu bytes\n", sizeof(int));
   printf("Tamanho de long int: %lu bytes\n", sizeof(long int));
+  printf("Tamanho de unsigned long int: %lu bytes\n", sizeof(unsigned long int));
   printf("Tamanho de long long int: %lu bytes\n", sizeof(long long int));
   printf("Tamanho de double: %lu bytes\n", sizeof(double));
   printf("Tamanho de long double: %lu bytes\n", sizeof(long double));
